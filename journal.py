0e649e181e06659edb7fa3ef64480f428ef64d6a
diawest82 <diallowest@gmail.com>
2017-05-29 11:16:44 -0500

    Update journal.py

--- journal.py
+++ journal.py
@@ -1,9 +1,8 @@
 from flask import (Flask, g, render_template, redirect, url_for, flash, abort,
-                   request
-)
+                   request)
 from flask.ext.bcrypt import check_password_hash
 from flask.ext.login import (LoginManager, login_user, current_user,
-                            logout_user, login_required)
+                             logout_user, login_required)
 import unidecode
 import re
 
@@ -11,8 +10,8 @@ import forms
 import models
 
 DEBUG = True
-HOST = '0.0.0.0'
 PORT = 8000
+HOST = '0.0.0.0'
 
 app = Flask(__name__)
 app.secret_key = "l;kjdfa./m1lkjJJK#jk;kj0agm;"
@@ -21,16 +20,19 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+
 @app.template_filter()
 def slugify(title):
     """Turns a title into the slug of a url."""
     title = unidecode.unidecode(title).lower()
     return re.sub(r'\W+', '-', title)
 
+
 @app.template_filter()
 def split_string(string, delimter=','):
     """Split a string by the comma."""
     return string.strip().split(delimter)
+
 
 @login_manager.user_loader
 def load_user(userid):
@@ -39,12 +41,14 @@ def load_user(userid):
     except models.DoesNotExist:
         return None
 
+
 @app.before_request
 def before_request():
     """Connect to the database before each request."""
     g.db = models.DATABASE
     g.db.connect()
     g.user = current_user
+
 
 @app.after_request
 def after_request(response):
@@ -61,12 +65,16 @@ def index():
     each journal entry. Include a link to add an entry. If not: sent to
     welcome page.
     """
-    if current_user in models.User.select():
-        user = current_user
-        journal = user.get_journal()
-        return render_template('journal.html', journal=journal, user=user)
+    try:
+        if current_user in models.User.select():
+            user = current_user
+            stream = user.get_entry().limit(10)
+            return render_template('journal.html', user=user, stream=stream)
+    except models.DoesNotExist:
+        pass
     else:
         return render_template('welcome.html')
+
 
 @app.route('/register', methods=('POST', 'GET'))
 def register():
@@ -98,6 +106,7 @@ def login():
                 flash("The username or password doesn't match.", "error")
     return render_template('login.html', form=form)
 
+
 @app.route('/logout')
 @login_required
 def logout():
@@ -106,77 +115,76 @@ def logout():
     return redirect(url_for('index'))
 
 
-#current problem: linking to this from a different template
-@app.route('/new', methods=('GET', 'POST'))
+@app.route('/new', methods=['GET', 'POST'])
 @login_required
-def create_entry():
+def new():
     form = forms.BlogForm()
     if form.validate_on_submit():
         models.BlogEntry.create_entry(title=form.title.data,
-                                    date=form.date.data,
-                                    time_spent=form.time_spent.data,
-                                    learned=form.learned.data,
-                                    resources=form.resources.data,
-                                    tags=tags,
-                                    user=g.user._get_current_object(),
-                                    slug=slugify(form.title.data),
-                            )
+                                      date=form.date.data,
+                                      time_spent=form.time_spent.data,
+                                      learned=form.learned.data,
+                                      resources=form.resources.data,
+                                      tags=form.tags.data,
+                                      user=g.user._get_current_object(),)
         flash("New entry has been added!", "success")
         return redirect(url_for('index'))
     return render_template('new.html', form=form)
 
-#current editing
-@app.route('/detal/')
-@app.route('/detail/<int:blogentry_id>/<string:slug>')
-def view_blog(blogentry_id, slug):
+
+@app.route('/detail')
+@app.route('/detail/<int:blogid>/<string:slug>')
+def detail(blogid, slug):
     try:
-        entry = models.Entry.select().where(models.BlogEntry.id==blogentry_id).get()
+        entry = models.BlogEntry.select().where(
+            models.BlogEntry.pk == blogid).get()
     except models.BlogEntry.DoesNotExist:
-        abort(404)
+        return "That entry does not exist."
     return render_template('detail.html', entry=entry)
+
 
 @app.route('/tags/<string:tags>')
 @login_required
-def tags(tag):
+def tags(tags):
     """Shows Journal entry tags"""
     user = current_user
-    journal = get_tags(tag)
-    return render_template("journal.html", journal=journal, user=user)
+    stream = user.get_tags(tags)
+    return render_template("journal.html", stream=stream, user=user, tags=tags)
 
-#currently editing this
-@app.route('/edit')
-@app.route('/edit/', methods=('GET', 'POST'))
+
+@app.route('/edit/<int:blogid>', methods=('GET', 'POST'))
 @login_required
-def edit_entry():
+def edit(blogid=None):
     """
     Allows a uder to edit a journal entry
     """
     try:
-            entry = models.BlogEntry.select().where(models.BlogEntry.id==blogentry_id).get()
+        entry = models.BlogEntry.select().where(
+            models.BlogEntry.pk == blogid).get()
     except models.DoesNotExist:
-        render_template('edit.html')
+        return render_template('edit.html', form=None, entry=None)
     else:
-        form = forms.BlogEntryForm(obj=entry)
+        form = forms.BlogForm(obj=entry)
         if request.method == 'POST':
             if form.validate_on_submit():
-                if form.title.data != entry.title:
-                    entry.slug = slugify(form.title.data)
                 entry.title = form.title.data
                 entry.date = form.date.data
                 entry.time_spent = form.time_spent.data
-                entry.learning = form.learning.data
-                entry.resources = form.resources
-                entry.tags = form.tags
+                entry.learned = form.learned.data
+                entry.resources = form.resources.data
+                entry.tags = form.tags.data
                 entry.save()
                 flash("The Journal has been updataed!", "success")
-                return redirect(url_for('details', entry_id=entry.id, slug=entry.slug))
+                return redirect(url_for('index'))
     return render_template('edit.html', form=form, entry=entry)
 
-@app.route('/delete/<int:blogentry_id>')
+
+@app.route('/delete/<int:blogid>')
 @login_required
-def delete_entry(blogentry_id):
+def delete(blogid):
     try:
-        entry = models.BlogEntry.select().where(models.BlogEntry.id == blogentry_id).get()
+        entry = models.BlogEntry.select().where(
+            models.BlogEntry.pk == blogid).get()
     except models.DoesNotExist:
         abort(404)
     else:
@@ -194,4 +202,4 @@ if __name__ == '__main__':
         )
     except ValueError:
         pass
-    app.run(debug=DEBUG, host=HOST, port=PORT)
+    app.run(debug=DEBUG, port=PORT, host=HOST)
